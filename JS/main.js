51725157b8a75752fdd9186bc8417a950fa01d4f
Emanuele D'Alessandro <dalessandroemanuelek@gmail.com>
2022-11-29 15:49:35 +0100

    workin on adding tasks

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,62 @@
+// riprodurre la ToDoList come viste insieme questa mattina,
+// ma con una struttura di array di oggetti e non un array di semplici stringhe.
+// l’array di oggetti di partenza può essere una cosa così:
+// tasks: [
+// { text: 'Fare i compiti', done: false }, 
+// { text: 'Fare la spesa', done: true }, 
+// { text: 'Fare il bucato', done: false }
+// ]
+// Bonus possibili (qualche idea ma sperimentate che volete):
+// curare l’interfaccia e renderla più carina e/o funzionale;
+// gestire anche lo stato “done” del task;
+// far succedere qualcosa al completamento del caricamento dell’applicazione;
+// etc..
 
+
+const {createApp} = Vue
+
+createApp({
+    data(){
+        return{
+            tasks: [
+            { text: 'Fare i compiti', done: false }, 
+            { text: 'Fare la spesa', done: true }, 
+            { text: 'Fare il bucato', done: false }
+            ]
+
+        }
+    },
+    methods:{
+        
+    }
+}).mount('#app')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const {createApp} = Vue
+
+// createApp({
+//     data(){
+//         return{
+
+//         }
+//     },
+//     methods:{
+        
+//     }
+// }).mount('#app')
 
 
 
